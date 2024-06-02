79b16bc1c0c16db6595477735e77eb6a48e5a38d
Yttehs <shetty.yttehs@outlook.com>
2024-06-02 11:44:25 +0800

    Add x86_64 platform

---
+++ build-x86_64.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+DEFCONFIG="allnoconfig"
+O="out"
+ARCH="x86_64"
+
+GREEN="\033[0;32m"
+BLUE="\033[0;34m"
+YELLOW="\033[0;33m"
+RED="\033[0;31m"
+RESET="\033[0m"
+
+# clean environment
+echo -e "${YELLOW}-> make mrproper ...${RESET}"
+rm -rf $O
+make mrproper
+
+# build kernel
+echo -e "${YELLOW}-> make ${DEFCONFIG} ...${RESET}"
+mkdir -p $O
+make O=$O ARCH=$ARCH $DEFCONFIG
+
+echo -e "${YELLOW}-> make ...${RESET}"
+make -j$(nproc --all) O=$O \
+                      ARCH=$ARCH \
+                      CROSS_COMPILE=
+
+cp $O/busybox .
